66b7fc2ee1660df095a3b03a0d96325844b00239
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-13 14:41:26 +0200

    edited conditionService
    currently the server does not allow requests from external sources.
    This will not be a problem later as the map will be included on the LoA viewer

--- src/services/conditionService.ts
+++ src/services/conditionService.ts
@@ -1,15 +1,44 @@
 import axios from 'axios';
+import conditionsData from '../data/conditions.json';
+import Condition from '../interfaces/condition.interface';
+import waypointsData from '../data/waypoints.json';
+import Waypoint from '../interfaces/waypoint.interface';
+import coordinates from '../utils/coordinates';
+import L from 'leaflet';
 
-async function getConditions() {
-    try {
-        const response = await axios.get('https://loa.vatsim-germany.org/api/v1/conditions')
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
+// server does not allow API get requests due to CORS policy, using static json to simulate API responses
+// async function getConditions() {
+//     try {
+//         const response = await axios.get('https://loa.vatsim-germany.org/api/v1/conditions')
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// }
+
+function getConditions() {
+    const conditions = conditionsData as Condition[];
+    return conditions;
+}
+
+function getCoordinates(name: string)
+{
+    const waypoints = waypointsData as Waypoint[];
+    const waypoint = waypoints.find((waypoint) => waypoint.name === name);
+
+    if (waypoint === undefined)
+    {
+        return undefined;
     }
+
+    const latitude = coordinates.convertLatitudeToNumber(waypoint.LAT);
+    const longitude = coordinates.convertLongitudeToNumber(waypoint.LONG);
+
+    return [L.latLng(latitude, longitude)];
 }
 
 export default {
     getConditions,
+    getCoordinates,
 };
