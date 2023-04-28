b9f16868d1c519b4be891258411e593a28601ddb
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-28 19:16:19 +0200

    added custom marker

---
+++ src/components/CustomMarker.tsx
@@ -0,0 +1,65 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+import Condition from "../interfaces/condition.interface";
+import { DivIcon } from "leaflet";
+import { renderToStaticMarkup } from 'react-dom/server';
+import conditionService from "../services/conditionService";
+import { Marker } from "react-leaflet";
+
+interface Props {
+    cop: string;
+    conditions: Condition[];
+}
+
+function ConditionTableIcon({cop, conditions} : Props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>AD</th>
+          <th>ADEP/ADES</th>
+          <th>COP</th>
+          <th>Level</th>
+          <th>XC</th>
+          <th>Special Conditions</th>
+          <th>From Sector</th>
+          <th>To Sector</th>
+          <th>From FIR</th>
+          <th>To FIR</th>
+        </tr>
+      </thead>
+      <tbody>
+        {conditions.map((condition, index) => (
+          <tr key={index}>
+            <td>{condition.aerodrome}</td>
+            <td>{condition.adep_ades}</td>
+            <td>{condition.cop}</td>
+            <td>{condition.level}</td>
+            <td>{condition.xc}</td>
+            <td>{condition.special_conditions}</td>
+            <td>{condition.from_sector}</td>
+            <td>{condition.to_sector}</td>
+            <td>{condition.from_fir}</td>
+            <td>{condition.to_fir}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function ConditionMarker({cop, conditions} : Props) {
+
+  const customIcon = new DivIcon({className: 'custom-icon', html: renderToStaticMarkup(<ConditionTableIcon cop={cop} conditions={conditions}/>)});
+  const copCoords = conditionService.getCoordinates(cop);
+
+  return (
+    <>
+    {copCoords && (
+      <Marker position={copCoords} icon={customIcon}></Marker>
+    )}
+    </>
+  );
+}
+
+export default ConditionMarker
+
