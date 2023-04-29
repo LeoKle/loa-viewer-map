3ce92e6f7903f8551a689f7f60b0a959543912ec
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-29 19:35:44 +0200

    stylized search bar using Prime React

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ import {
 
 import "./App.css";
 import LeafletReactMap from "./components/LeafletReactMap";
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={<LeafletReactMap/>}
-          />
+          <Route path="/" element={<LeafletReactMap />} />
         </Routes>
       </Router>
     </>
--- src/components/LeafletReactMap.tsx
+++ src/components/LeafletReactMap.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Polygon, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Condition from "../interfaces/condition.interface";
-import { LatLng, LatLngExpression } from "leaflet";
 import useDebounce from "../hooks/useDebounce";
 import ConditionMarker from "./CustomMarker";
 import conditionService from "../services/conditionService";
 import { SectorJson } from "../interfaces/apisectors.interface";
 import filterConditionsService from "../services/filterConditionsService";
 import groupConditionsByCop from "../services/groupConditionsService";
+import { InputText } from "primereact/inputtext";
 
 export default function LeafletReactMap() {
   const [center, setCenter] = useState<LatLngExpression>([50.026292, 8.765245]);
@@ -38,29 +38,38 @@ export default function LeafletReactMap() {
 
   return (
     <>
-      <input
-        type="search"
-        placeholder="Search"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        style={{ width: "100vw", height: "100vh" }}
-        maxZoom={10}
-        minZoom={6}
-      >
-        <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+      <div style={{ position: "relative" }}>
+        <InputText
+          type="search"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+          style={{
+            position: "absolute",
+            top: "5%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 1,
+          }}
         />
-        {filteredConditions &&
-          Object.entries(filteredConditions).map(([cop, conditions]) => (
-            <>
-              <ConditionMarker conditions={conditions} cop={cop} />
-            </>
-          ))}
-      </MapContainer>
+        <MapContainer
+          center={center}
+          zoom={zoom}
+          style={{ width: "100vw", height: "100vh", zIndex: 0 }}
+          maxZoom={10}
+          minZoom={6}
+        >
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          />
+          {filteredConditions &&
+            Object.entries(filteredConditions).map(([cop, conditions]) => (
+              <>
+                <ConditionMarker conditions={conditions} cop={cop} />
+              </>
+            ))}
+        </MapContainer>
+      </div>
     </>
   );
 }
