e79440b3a2b659c75ff8f6a29288b5d463686c5a
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-30 14:58:03 +0200

    removed labels from Map

--- src/components/LeafletReactMap.tsx
+++ src/components/LeafletReactMap.tsx
@@ -60,7 +60,7 @@ export default function LeafletReactMap() {
           minZoom={6}
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           />
           {filteredConditions &&
